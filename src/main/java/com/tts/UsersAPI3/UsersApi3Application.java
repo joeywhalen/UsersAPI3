db824f9e0383c738814ca6a894d206423d9c0aab
Joey Whalen <jtktwhalen@gmail.com>
2021-03-26 18:04:36 -0400

    initial commit

---
+++ src/main/java/com/tts/UsersAPI3/UsersApi3Application.java
@@ -0,0 +1,13 @@
+package com.tts.UsersAPI3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApi3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApi3Application.class, args);
+	}
+
+}
